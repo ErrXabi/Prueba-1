2a46829acc3cb411b7be52ac9b6ae02e21e2898a
javier <ikcec@plaiaundi.net>
2024-12-11 10:30:18 +0100

    COMMMIT

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola rama 1");
+        System.out.println("Hola FGSDDFGFGDFGB 1");
     }
 }
